9227136e4753b42f10feb552635e6f970ebb60cc
Timon Reich <timon.reich@gmx.de>
2022-01-30 18:01:10 +0100

    Template

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ import led_strip as ls
 if __name__ == '__main__':
 
     #strip = LedStrip()
-    #strip.begin()
+    #strip.init()
 
     try:
 
@@ -16,4 +16,4 @@ if __name__ == '__main__':
         pass # nix
         # statt nix bitte dunkel machen
 
-
+    #strip.exit()
