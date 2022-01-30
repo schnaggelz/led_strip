721fb823da1014fe96f1f761aadab325875f1614
Timon Reich <timon.reich@gmx.de>
2022-01-30 17:48:54 +0100

    Template

--- hello_world.py
+++ hello_world.py
@@ -3,10 +3,7 @@ import led_strip as ls
 # Main program logic follows:
 if __name__ == '__main__':
 
-    ls.
-
-    #strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-
+    #strip = LedStrip()
     #strip.begin()
 
     try:
