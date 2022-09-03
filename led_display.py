cdf7f2954147050a310ba94f5ecbf2b40ea3c90b
Timon Reich <timon.reich@gmx.de>
2022-09-03 19:06:00 +0200

    Use patterns

--- led_display.py
+++ led_display.py
@@ -3,28 +3,15 @@ import time
 from led_matrix import LedMatrix
 from neopixel import Color
 
-# import from character_patterns.py later on
-character_patterns = \
-{
-   'A': 
-   [ 
-        0b000000,
-        0b011100,
-        0b100010,
-        0b100010,
-        0b111110,
-        0b100010,
-        0b100010,
-        0b100010 
-   ], # A
-}
+import character_patterns
+
 
 class LedDisplay(LedMatrix):
     def __init__(self):
         super().__init__()
 
     def print_character(self, character, color=Color(255, 200, 10), column_offset=0, num_cols=6):
-        character_pattern = character_patterns[character]
+        character_pattern = character_patterns.PATTERNS[character]
         for row_index in range(self.LED_ROWS):
             row_pattern = character_pattern[row_index]
             row_bits = [(row_pattern >> line_bit) & 1 for line_bit in range(num_cols - 1, -1, -1)]
@@ -35,6 +22,12 @@ class LedDisplay(LedMatrix):
                 else:
                     super().clear_color(row_index, column_offset + column_index)
 
+    def test_all_characters(self):
+        for character in character_patterns.PATTERNS:
+            self.print_character(character)
+            self.show()
+            time.sleep(2)
+
 
 if __name__ == '__main__':
 
@@ -42,10 +35,7 @@ if __name__ == '__main__':
     display.init()
     display.set_brightness(10)
 
-    display.print_character('A')
-
-    display.show()
-    time.sleep(2)
+    display.test_all_characters()
     
     display.clear()
     display.exit()
