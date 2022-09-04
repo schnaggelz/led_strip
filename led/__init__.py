9159e6e4197a042fada16a02b7d7347c11745f4a
Timon Reich <timon.reich@gmx.de>
2022-09-04 18:58:22 +0200

    Some more playground

--- led/__init__.py
+++ led/__init__.py
@@ -1,4 +1,5 @@
+from .neopixel import Color
 from .led_display import LedDisplay
 from .led_matrix import LedMatrix
 from .led_strip import LedStrip
-from .neopixel import Color
+from .led_word_rotate import LedWordRotate
